7f9628a17f8727bea2e1dca7c7c2bcc5d0631521
almousleck <microservicestech97@gmail.com>
2024-10-03 17:31:32 +0800

    added show btn sheet call service number

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -3,6 +3,7 @@ import 'package:ui_trip/screens/passenger_information_screen.dart';
 import 'package:ui_trip/screens/version_screen.dart';
 import 'package:ui_trip/widgets/profile_app_bar.dart';
 import 'package:ui_trip/widgets/reset_email.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../service/auth_service.dart';
 import '../widgets/show_dialogs.dart';
@@ -12,11 +13,51 @@ class ProfileScreen extends StatelessWidget {
 
   final AuthService _authService = AuthService();
 
+  // this a single method to logout
   void _logout(BuildContext context) async {
     bool shouldLogout = await showLogoutConfirmation(context);
     if (shouldLogout) {
       await _authService.logout();
       Navigator.pushReplacementNamed(context, '/login');
+    }
+  }
+
+  // this method show thr call and cancel btn
+  void _showCallBottomSheet(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.call),
+              title: Text('Call +8613027207507'),
+              onTap: () {
+                Navigator.pop(context); // Close the bottom sheet
+                _makePhoneCall('+8613027207507');
+              },
+            ),
+            ListTile(
+              leading: Icon(Icons.cancel),
+              title: Text('Cancel'),
+              onTap: () {
+                Navigator.pop(context); // Close the bottom sheet
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // this is the method to call a number
+  void _makePhoneCall(String phoneNumber) async {
+    final Uri phoneUri = Uri(scheme: 'tel', path: phoneNumber);
+    if (await canLaunchUrl(phoneUri)) {
+      await launchUrl(phoneUri);
+    } else {
+      throw 'Could not launch $phoneNumber';
     }
   }
 
@@ -72,11 +113,17 @@ class ProfileScreen extends StatelessWidget {
                     trailing: Icon(Icons.chevron_right),
                   ),
                   const Divider(),
-                  const ListTile(
-                    leading: Icon(Icons.phone),
-                    title: Text('Mobile number'),
-                    subtitle: Text('24/7'),
-                    trailing: Icon(Icons.chevron_right),
+                  //ShowCallBottomSheet(),
+                  GestureDetector(
+                    onTap: () {
+                      _showCallBottomSheet(context);
+                    },
+                    child: const ListTile(
+                      leading: Icon(Icons.phone),
+                      title: Text('Mobile number'),
+                      subtitle: Text('24/7'),
+                      trailing: Icon(Icons.chevron_right),
+                    ),
                   ),
                   const Divider(),
                   GestureDetector(
@@ -111,41 +158,3 @@ class ProfileScreen extends StatelessWidget {
     );
   }
 }
-
-// body: Expanded(
-//         child: ListView(
-//           padding: const EdgeInsets.all(8),
-//           children: const [
-//             ListTile(
-//               leading: Icon(Icons.email),
-//               title: Text('My email'),
-//               trailing: Icon(Icons.chevron_right),
-//             ),
-//             Divider(),
-//             ListTile(
-//               leading: Icon(Icons.people),
-//               title: Text('Passenger'),
-//               trailing: Icon(Icons.chevron_right),
-//             ),
-//             Divider(),
-//             ListTile(
-//               leading: Icon(Icons.lock),
-//               title: Text('Change password'),
-//               trailing: Icon(Icons.chevron_right),
-//             ),
-//             Divider(),
-//             ListTile(
-//               leading: Icon(Icons.phone),
-//               title: Text('Mobile number'),
-//               subtitle: Text('24/7'),
-//               trailing: Icon(Icons.chevron_right),
-//             ),
-//             Divider(),
-//             ListTile(
-//               leading: Icon(Icons.info),
-//               title: Text('Version'),
-//               trailing: Text('5.8.2.31'),
-//             ),
-//           ],
-//         ),
-//       ),
